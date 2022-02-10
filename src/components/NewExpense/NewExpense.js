15afe0189f526603540cbfca71aabf133c03003b
geekshikha <sssinghshikha8@gmail.com>
2022-02-10 10:24:50 +0100

    items based on year

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,12 +5,19 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
-  return (
-    <div className='new-expense'>
-     <ExpenseForm />
-     
-    </div>
-  );
-};
-
-export default NewExpense;
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+      const expenseData = {
+        ...enteredExpenseData,
+        id: Math.random().toString()
+      };
+      props.onAddExpense(expenseData);
+    };
+  
+    return (
+      <div className='new-expense'>
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      </div>
+    );
+  };
+  
+  export default NewExpense;
